0c07983658d3f90117bd6c788cd4f2211225306b
Vladimir Dubos <dubos1210@yandex.ru>
2018-07-24 15:14:19 +0300

    Everything updated

---
+++ 328/24C0x.h
@@ -0,0 +1,49 @@
+﻿/* 24C01/24C02 external EEPROM Library for Atmel AVR v0.1
+ * EXTEEPROM_writeByte(0b10101110, 0x00, 0xFF);  //Запись значения 0xFF в ячейку 0x00 микросхемы с адресом 0b10101110
+ * EXTEEPROM_readByte(0b10101110, 0x00);		 //Чтение из ячейки 0x00 микросхемы 0b10101110
+ * 
+ * Адрес микросхемы: старшие четыре бита - 1010, затем уровни на выводах A2, A1 и A0, затем ноль.
+ * Например: A2 подключен к нулю; A1, A0 подключены к питанию. Тогда адрес - 0b10100110
+ *
+ * Нога WP: ноль - запись разрешена; единица - запись запрещена
+ */
+
+#ifndef DUBOS_EXTEEPROM
+#define DUBOS_EXTEEPROM
+
+#include "I2C.h"
+
+/***************************************
+ * Функция		: запись байта
+ * Аргументы	: адрес микросхемы,
+				  адрес ячейки памяти,
+				  байт для записи
+ ****************************************/
+void EXTEEPROM_writeByte(unsigned char ic_addr, unsigned char addr, unsigned char data) {
+	I2C_start();
+	I2C_transmit(ic_addr);
+	I2C_transmit(addr);
+	I2C_transmit(data);
+	I2C_stop();
+}
+
+/***************************************
+ * Функция		: запись байта
+ * Аргументы	: адрес микросхемы,
+				  адрес ячейки памяти
+ * Ответ		: считанный байт
+ ****************************************/
+unsigned char EXTEEPROM_readByte(unsigned char ic_addr, unsigned char addr) {
+	unsigned char data = 0;
+	I2C_start();
+	I2C_transmit(ic_addr);
+	I2C_transmit(addr);
+	I2C_start();
+	I2C_transmit(ic_addr + 1);
+	data = I2C_receive();
+	I2C_stop();
+	
+	return data;
+}
+
+#endif
---
+++ 328/SWSPI.h
@@ -0,0 +1,67 @@
+﻿/* Software SPI Library for Atmel AVR v0.3
+ *  SWSPI_init();         //Инициализация SPI
+ *  SWSPI_byteTxRx(0x**); //Отправка байта 0x**. Возвращает принятые данные
+ *  
+ *  Не забудьте отконфигурировать выводы и настройки в строках #define
+ */
+
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
+
+#ifndef DUBOS_SWSPI
+#define DUBOS_SWSPI
+
+#define SWSPI_DELAY		5		//Четверть периода SCK, мкс
+
+#define SPI_PORT		PORTB	//PORT регистр
+#define SPI_PIN			PINB	//PIN регистр
+#define SPI_DDR			DDRB	//DDR регистр
+
+#define SPI_MOSI		3		//Вывод MOSI
+#define SPI_MISO		4		//Вывод MISO
+#define SPI_SCK			5		//Вывод SCK
+#define SPI_CS			2		//Вывод CS
+
+
+#define SWSPI_SelectChip()			SPI_PORT &=~ (1<<SPI_CS)
+#define SWSPI_DeselectChip()		SPI_PORT |= (1<<SPI_CS)
+
+/*********************************************
+ * Функция	: инициализация программного SPI
+ *********************************************/
+void SWSPI_init(void) {
+	SPI_DDR |= (1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_CS);
+	SPI_DDR &=~ (1<<SPI_MISO);
+	
+	SPI_PORT |= (1<<SPI_CS)|(1<<SPI_MOSI);
+	SPI_PORT &=~ (1<<SPI_SCK);
+}
+
+/*********************************************
+ * Функция		: отправка и прием байта
+ * Аргументы	: байт для отправки
+ * Ответ		: принятый байт
+ *********************************************/
+unsigned char SWSPI_byteTxRx(unsigned char data) {
+	unsigned char answ = 0x00;
+	for(int SWSPI_i = 0; SWSPI_i < 8; SWSPI_i++) {
+		if(data & (1<<7)) {			
+			SPI_PORT |= (1<<SPI_MOSI);
+		}
+		else {			
+			SPI_PORT &=~ (1<<SPI_MOSI);
+		}
+		data = data << 1;
+		_delay_us(SWSPI_DELAY);
+		_delay_us(SWSPI_DELAY);
+		SPI_PORT |= (1<<SPI_SCK);
+		_delay_us(SWSPI_DELAY);
+		answ += SPI_PIN & SPI_MISO;
+		answ = answ << 1;
+		_delay_us(SWSPI_DELAY);
+		SPI_PORT &=~ (1<<SPI_SCK);
+	}
+}
+
+#endif
